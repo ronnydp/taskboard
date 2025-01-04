55681447d13090d14b72dee66676cc8b5fdae605
ronnydp <ronnydp742@gmail.com>
2025-01-04 01:00:19 -0500

    taskboard 1.7

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const PORT = process.env.PORT || 3000;
 
 export const DB_HOST = process.env.DB_HOST || 'localhost';
-export const DB_USER = process.env.DB_USER || 'postgress';
+export const DB_USER = process.env.DB_USER || 'ronny';
 export const DB_PASSWORD = process.env.DB_PASSWORD || '123456';
 export const DB_DATABASE = process.env.DB_DATABASE || 'taskboard';
 export const DB_PORT = process.env.DB_PORT || '5432';
